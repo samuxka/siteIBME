946bddb7b7c520003a600cb0bc8f3cd5927b8d19
Samuel <109113326+samuxka@users.noreply.github.com>
2023-12-30 11:35:13 -0300

    acaboooooooooooooooooou

--- javascript/script.js
+++ javascript/script.js
@@ -3,3 +3,22 @@ const header = document.querySelector("header")
 window.addEventListener("scroll", function() {
     header.classList.toggle("sticky", window.scrollY > 60)
 });
+
+function abrirImagem(imagem) {
+    // Cria um elemento de imagem maior
+    var imagemAmpliada = document.createElement("img");
+    imagemAmpliada.src = imagem.src;
+
+    // Cria uma div para a imagem ampliada
+    var divAmpliada = document.createElement("div");
+    divAmpliada.classList.add("imagem-ampliada");
+    divAmpliada.appendChild(imagemAmpliada);
+
+    // Adiciona a div ao corpo do documento
+    document.body.appendChild(divAmpliada);
+
+    // Adiciona um evento de clique para fechar a imagem ampliada
+    divAmpliada.onclick = function () {
+        document.body.removeChild(divAmpliada);
+    };
+}
