19600ac5e0ca349251bcc7f823b05b729817cf1d
Samuel <109113326+samuxka@users.noreply.github.com>
2023-12-30 12:42:47 -0300

    agr ss... acabooooooooooooooo

--- javascript/script.js
+++ javascript/script.js
@@ -22,3 +22,12 @@ function abrirImagem(imagem) {
         document.body.removeChild(divAmpliada);
     };
 }
+
+
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('open');
+};
